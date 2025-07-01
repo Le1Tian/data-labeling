0c2f619ae05e1c61f8b7f7ec39ee870adbaf785d
Le1Tian <121712392+Le1Tian@users.noreply.github.com>
2025-07-01 10:05:36 +0800

    init

---
+++ src/main/java/com/tl/datalabeling/DataLabelingApplication.java
@@ -0,0 +1,13 @@
+package com.tl.datalabeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLabelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLabelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tl/datalabeling/DataLabelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.datalabeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLabelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
